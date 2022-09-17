0e0bcdb04c98736d4c58a6ec8cefc65ee3fa5af4
HitomiTahara <hitomi.20220218@gmail.com>
2022-09-17 21:58:59 +0900

    Add validation for new.php(cosmelog)

--- src/create.php
+++ src/create.php
@@ -35,6 +35,19 @@ EOT;
   }
 }
 
+function validate($cosme)
+{
+
+  $errors = [];
+
+  if (!strlen($cosme['product-name'])) {
+    $errors['product-name'] = '化粧品名を入れてください';
+  } elseif (strlen($cosme['product-name']) > 255) {
+    $errors['product-name'] = '化粧品名を255文字以内で入れてください';
+  }
+
+  return $errors;
+}
 
 //HTTPメソッドがPOSTだったら
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
@@ -48,20 +61,87 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 
   //バリデーション
-  $link = dbConnect();
-  createCosme($link, $cosme);
-  mysqli_close($link);
+  // バリデーションする
+  $errors = validate($cosme);
 
-  // //データベースに接続
-  $link = dbConnect();
-  // //データベースにデータを登録する
+  if (!count($errors)) {
+    $link = dbConnect();
+    createCosme($link, $cosme);
+    mysqli_close($link);
 
-  createCosme($link, $cosme);
-
-  // //データベースとの接続を切断
-  mysqli_close($link);
+    // //データベースに接続
+    $link = dbConnect();
+    // //データベースにデータを登録する
+    createCosme($link, $cosme);
+    // //データベースとの接続を切断
+    mysqli_close($link);
+    //リダイレクト処理
+    header("location: index.php");
+  }
 }
+?>
 
-//リダイレクト処理
+<!DOCTYPE html>
+<html lang="ja">
 
-header("location: index.php");
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>コスメログ</title>
+</head>
+
+<body>
+  <h1>化粧品の登録</h1>
+  <form action="create.php" method="POST">
+    <?php if (count($errors)) : ?>
+      <ul>
+        <?php foreach ($errors as $error) : ?>
+          <li><?php echo $error; ?> </li>
+        <?php endforeach; ?>
+      </ul>
+    <?php endif; ?>
+    <div>
+      <label for="product-name">化粧品名</label>
+      <input type="text" name="product-name" id="product-name">
+    </div>
+
+    <div>
+      <label for="product-maker">メーカー名</label>
+      <input type="text" name="product-maker" id="product-maker">
+    </div>
+
+    <div>
+      <label>使用状況</label>
+      <div>
+        <div>
+          <input type="radio" name="use-by-date" id="year" value="１年">
+          <label for="year">１年</label>
+        </div>
+        <div>
+          <input type="radio" name="use-by-date" id="half-year" value="半年">
+          <label for="half-year">半年</label>
+        </div>
+        <div>
+          <input type="radio" name="use-by-date" id="not-use" value="未使用">
+          <label for="not-use">未使用</label>
+        </div>
+      </div>
+    </div>
+
+    <div>
+      <label for="suggestion">おすすめ度（10満点の整数）</label>
+      <input type="number" name="suggestion" id="suggestion">
+    </div>
+
+    <div>
+      <label for="etc">備考</label>
+      <textarea type="text" id="etc" name="etc" rows="10"></textarea>
+    </div>
+
+
+    <button type="submit">登録する</button>
+  </form>
+
+</body>
+
+</html>
