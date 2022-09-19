e437649270c7249e4dd3e66106c1591cd29f7457
HitomiTahara <hitomi.20220218@gmail.com>
2022-09-19 17:04:23 +0900

    Add Bootstrap

--- src/vendor/composer/autoload_static.php
+++ src/vendor/composer/autoload_static.php
@@ -18,6 +18,7 @@ class ComposerStaticInit1dcdf25181e41c3fad4d93664d536134
             'Symfony\\Polyfill\\Php80\\' => 23,
             'Symfony\\Polyfill\\Mbstring\\' => 26,
             'Symfony\\Polyfill\\Ctype\\' => 23,
+            'ScssPhp\\ScssPhp\\' => 16,
         ),
         'P' => 
         array (
@@ -45,6 +46,10 @@ class ComposerStaticInit1dcdf25181e41c3fad4d93664d536134
         'Symfony\\Polyfill\\Ctype\\' => 
         array (
             0 => __DIR__ . '/..' . '/symfony/polyfill-ctype',
+        ),
+        'ScssPhp\\ScssPhp\\' => 
+        array (
+            0 => __DIR__ . '/..' . '/scssphp/scssphp/src',
         ),
         'PhpOption\\' => 
         array (
