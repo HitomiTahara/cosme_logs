781e0713cbab656dcd1661279a1f91c74e1517c0
HitomiTahara <hitomi.20220218@gmail.com>
2022-09-22 13:30:58 +0900

    Add list page for cosme and company

--- src/index.php
+++ src/index.php
@@ -1,5 +1,26 @@
 <?PHP
 
+require_once __DIR__ . '/lib/escape.php';
+require_once __DIR__ . '/lib/mysqli.php';
+
+function listCosmelog($link)
+{
+    $cosme = [];
+
+    $sql = 'SELECT id, product_name,product_maker,use_by_date,suggestion,etc From cosmelog';
+    $results = mysqli_query($link, $sql);
+
+    while ($cos = mysqli_fetch_assoc($results)) {
+        $cosme[] = $cos;
+    }
+    mysqli_free_result($results);
+    return $cosme;
+}
+
+
+$link = dbConnect();
+$cosme = listCosmelog($link);
+
 $title = '化粧品ログの一覧';
 $content = __DIR__ . '/views/index.php';
 include __DIR__ . '/views/layout.php';
--- src/views/index.php
+++ src/views/index.php
@@ -3,13 +3,17 @@
 <a href="new.php" class="text-decoration-none  mb-4">化粧品ログを登録する</a>
 
 <main>
-    <section class="mb-2">
-        <h2>口紅</h2>
-        <div>KOSE</div>
-    </section>
-    <section class="mb-2">
-        <h2>口紅</h2>
-        <div>KOSE/１年/５点</div>
-        <p>秋色</p>
-    </section>
+    <?php if (count($cosme) > 0) : ?>
+        <?php foreach ($cosme as $cos) : ?>
+            <section class="mb-2">
+                <h2><?php echo escape($cos['product_name']); ?></h2>
+                <div>
+                    メーカー名：<?php echo escape($cos['product_maker']); ?><br>
+                    使用期限：<?php echo escape($cos['use_by_date']); ?><br>
+                    評価：<?php echo escape($cos['suggestion']); ?><br>
+                    備考：<?php echo escape($cos['etc']); ?>
+                </div>
+            </section>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </main>
